fb352bb036c2cae6b86039f7e9fbe0f6a02dc5b8
maxdbxl <maxdbxl@gmail.com>
2025-04-11 15:57:09 +0200

    Feat : Register Member & Form Error (Custom Validation)

--- src/app/pages/register-member/register-member.component.ts
+++ src/app/pages/register-member/register-member.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Button } from 'primeng/button';
 import { Calendar } from 'primeng/calendar';
@@ -9,9 +9,12 @@ import { FloatLabel } from 'primeng/floatlabel';
 import { InputText } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
 import { MessageService } from 'primeng/api';
+import { MemberService } from '../../services/member.service';
+import { map } from 'rxjs';
+import { FormErrorComponent } from '../../components/form-error/form-error.component';
 
 @Component({
-  imports: [Button, InputText, FloatLabel, Calendar, Card, Fieldset, ReactiveFormsModule, Select],
+  imports: [Button, InputText, FloatLabel, Calendar, Card, Fieldset, ReactiveFormsModule, Select, FormErrorComponent],
   templateUrl: './register-member.component.html',
   styleUrl: './register-member.component.scss'
 })
@@ -23,16 +26,44 @@ export class RegisterMemberComponent {
   // Toast
   messageService = inject(MessageService);
   //Requêtes vers l'API
+  memberService = inject(MemberService);
 
-  roles = [
-    { label: 'Member', value: 0},
-    { label: 'Admin', value: 1}
-  ]
+  // roles = [
+  //   { label: 'Member', value: 0},
+  //   { label: 'Admin', value: 1}
+  // ]
 
   registerForm = this.fb.group({
     username: [null, [Validators.required, Validators.maxLength(100)]],
-    email: [null, [Validators.required, Validators.maxLength(450), Validators.email]],
+    email: [null, [Validators.required, Validators.maxLength(450), Validators.email], [
+      (control: AbstractControl) => this.memberService.exists(control.value)
+      .pipe(map(v => v ? { exist: true} : null))
+    ]],
     password: [null, [Validators.required]],
-    role: [null, [Validators.required]]
-  })
+    confirmPassword: [null, [Validators.required]]
+  });
+
+
+  submit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    
+
+    //Soumettre le formulaire à l'API
+    //TODO : Ajouter Loader
+    this.memberService.register({...this.registerForm.value})
+    .subscribe({
+      next: () => {
+        this.messageService.add({severity: 'success', summary: 'La sauvegarde a été effectuée avec succès'});
+        this.router.navigate(['/']);
+        //TODO : Ajouter Loader
+      },
+      error: () => {
+        this.messageService.add({severity: 'error', summary: 'La sauvegarde a échoué'});
+        //TODO : Ajouter Loader
+      }
+    })
+  }
 }
--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { catchError, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
+  httpClient = inject(HttpClient);
+  
   constructor() { }
+
+  register(form: any) {
+    return this.httpClient.post(environment.baseApiUrl + '/member', form);
+  }
+
+  exists(email: string) {
+    return this.httpClient.head(environment.baseApiUrl + '/member', {
+      params: { email }
+    }).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    )
+  }
 }
