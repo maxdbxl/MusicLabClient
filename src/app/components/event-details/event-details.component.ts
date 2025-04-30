6ffd83aa3628c9e13dd7f2eefd182603b8046c71
maxdbxl <167008375+maxdbxl@users.noreply.github.com>
2025-05-01 00:21:00 +0200

    Feat : Event Details Card (modifying template)

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -4,11 +4,11 @@ import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { EventModel } from '../../models/event.model';
 import { EVENT_TYPE } from '../../constants/event-type.constants';
 import { EventPipe } from '../../pipes/event.pipe';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-event-details',
-  imports: [CardModule, EventPipe, CommonModule],
+  imports: [CardModule, EventPipe, CommonModule, DatePipe],
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.scss'
 })
--- src/app/constants/event-type.constants.ts
+++ src/app/constants/event-type.constants.ts
@@ -4,9 +4,29 @@ export const EVENT_TYPE : any = {
         label: 'Répétition',
         bgImg: '/bg-img-training.png'
     },
-    PersonalEvent: {
+    Performance: {
         color: '#e84d15',
         label: 'Représentation',
         bgImg: '/bg-img-show.png'
+    },
+    OrganizationalMeeting: {
+        color: '#aef26b',
+        label: 'Réunion organisationnelle',
+        bgImg: '/bg-img-orgameeting.jpg'
+    },
+    LogisticsMeeting: {
+        color: '#42b5e3',
+        label: 'Réunion logistique',
+        bgImg: '/bg-img-orgameeting.jpg'
+    },
+    PersonalEvent: {
+        color: '#b842e3',
+        label: 'Evènement personnel',
+        bgImg: '/bg-img-orgameeting.jpg'
+    },
+    Other: {
+        color: '#9c5beb',
+        label: 'Autre',
+        bgImg : '/bg-img-orgameeting.jpg'
     }
 }
